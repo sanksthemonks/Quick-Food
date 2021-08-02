d15a24e1650bee91df1b386146d2b7245f89f670
sanksthemonks <sankalpapj@hotmail.com>
2021-08-02 21:05:30 +0545

    User Registration and Login Successful

---
+++ app/config/passport.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+function init(passport) {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        //If Email Exists
+        const user = await User.findOne({ email: email })
+        if(!user) {
+            return done(null, false, { message: 'No user with this email' })
+        }
+
+        bcrypt.compare(password, user.password).then(match => {
+            if(match) {
+                return done(null, user, { message: 'Logged In Successfully' })
+            }
+            return done(null, false, { message: 'Wrong Email or Password' })
+        }).catch(err => {
+            return done(null, false, { message: 'Something Went Wrong' })
+        })
+    }))
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id)
+    })
+
+    passport.deserializeUser((id, done) => {
+        User.findById( id, (err, user) => {
+            done(err, user)
+        })
+    })
+
+}
+
+module.exports = init
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 //Database connection
 const url = 'mongodb://127.0.0.1:27017/food';
@@ -31,14 +32,22 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 *24 }
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 // Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //Global Middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
