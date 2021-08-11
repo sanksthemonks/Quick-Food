8f37940e1acc21427e1d6723d11be607bcc17090
sanksthemonks <39763651+sanksthemonks@users.noreply.github.com>
2021-08-11 11:50:56 +0545

    Payment Bug FIXED

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,6 +1,6 @@
 const Order = require('../../../models/order')
 const moment = require('moment')
-const stripe = require('stripe')('process.env.STRIPE_PRIVATE_KEY')
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 function orderController() {
     return {
